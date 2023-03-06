83e47a671de560dcb7cddb3050f82f9a874bcf3a
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-03-06 15:42:21 +0100

    changes for web hosting

--- src/server.js
+++ src/server.js
@@ -13,10 +13,25 @@ import cors from "cors";
 import { join } from "path";
 
 const server = Express();
-const port = 3001;
+const port = process.env.PORT;
 const publicFolderPath = join(process.cwd(), "./public");
-
-server.use(cors());
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
 server.use(Express.static(publicFolderPath));
 server.use(Express.json());
 server.use("/authors", authorsRouter);
